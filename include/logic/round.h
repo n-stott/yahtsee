2faa2ea122b2522fb4c0adc63f0112236167684b
Nikolas <nikolas.stott@hotmail.fr>
2023-06-16 15:29:56 +0200

    utility ids for hands, throws and rethrows

--- include/logic/round.h
+++ include/logic/round.h
@@ -9,6 +9,10 @@
 #include <iostream>
 #include <string>
 
+struct HandId {
+    u64 id;
+};
+
 struct Hand {
     std::array<u8, 5> values;
 
@@ -17,6 +21,26 @@ struct Hand {
         hand.values = values;
         hand.sort();
         return hand;
+    }
+
+    static Hand fromId(HandId id) {
+        Hand hand;
+        hand.values[4] = id.id % 6;
+        id.id /= 6;
+        hand.values[3] = id.id % 6;
+        id.id /= 6;
+        hand.values[2] = id.id % 6;
+        id.id /= 6;
+        hand.values[1] = id.id % 6;
+        id.id /= 6;
+        hand.values[0] = id.id % 6;
+        return hand;
+    }
+
+    HandId toId() const {
+        u64 id = 0;
+        for(int v : values) id = 6*id+v;
+        return HandId{id};
     }
 
     void sort() {
@@ -41,6 +65,10 @@ namespace std {
     };
 }
 
+struct ThrowId {
+    u64 id;
+};
+
 struct Throw {
     std::array<u8, 5> values {{ 0, 0, 0, 0, 0 }};
     u8 size = 0;
@@ -50,11 +78,59 @@ struct Throw {
         for(u8 i = 0; i < size; ++i) s += std::to_string((int)values[i]);
         return s;
     }
+
+    static Throw fromId(ThrowId id) {
+        Throw t;
+        t.values[4] = id.id % 6;
+        id.id /= 6;
+        t.values[3] = id.id % 6;
+        id.id /= 6;
+        t.values[2] = id.id % 6;
+        id.id /= 6;
+        t.values[1] = id.id % 6;
+        id.id /= 6;
+        t.values[0] = id.id % 6;
+        return t;
+    }
+
+    ThrowId toId() const {
+        u64 id = 0;
+        for(int i = 0; i < size; ++i) {
+            id = 6*id + values[i];
+        }
+        return ThrowId{id};
+    }
+};
+
+struct RethrowId {
+    u64 id;
 };
 
 struct Rethrow {
     std::array<u8, 5> keptValues {{ 0, 0, 0, 0, 0 }};
     u8 size = 0;
+
+    static Rethrow fromId(RethrowId id) {
+        Rethrow rt;
+        rt.keptValues[4] = id.id % 6;
+        id.id /= 6;
+        rt.keptValues[3] = id.id % 6;
+        id.id /= 6;
+        rt.keptValues[2] = id.id % 6;
+        id.id /= 6;
+        rt.keptValues[1] = id.id % 6;
+        id.id /= 6;
+        rt.keptValues[0] = id.id % 6;
+        return rt;
+    }
+
+    RethrowId toId() const {
+        u64 id = 0;
+        for(int i = 0; i < size; ++i) {
+            id = 6*id + keptValues[i];
+        }
+        return RethrowId{id};
+    }
 };
 
 inline Hand combine(const Rethrow& rt, const Throw& t) {
--- include/utils.h
+++ include/utils.h
@@ -2,5 +2,8 @@
 #define UTILS_H
 
 using u8 = unsigned char;
+using u64 = unsigned long long;
+
+static_assert(sizeof(u64) == 8);
 
 #endif
